8ab0b42dd9ae03257dffa494fbe290266f726815
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-12 21:03:07 +0200

    tetsing for exaple

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
+// import { App } from 'components/App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const user  = {
+  "username": "Jacques Gluke",
+  "tag": "jgluke",
+  "location": "Ocho Rios, Jamaica",
+  "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  "stats": {
+    "followers": 5603,
+    "views": 4827,
+    "likes": 1308
+  }
+}
+
+const profile = (
+  <div class="profile">
+  <div class="description">
+    <img
+      src={user.avatar}
+      alt={user.username}
+      class="avatar"
+    />
+    <p class="name">{user.username}</p>
+      <p class="tag">@{user.tag}</p>
+      <p class="location">{user.location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+        <span class="quantity">{user.stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+        <span class="quantity">{user.stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+        <span class="quantity">{user.stats.likes}</span>
+    </li>
+  </ul>
+  </div>
+)
+
+
+
+ReactDOM.render(profile, document.querySelector('#root'));
+
+
+
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
