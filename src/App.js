fc8a0463e5645dc4c91e41aa3a34eb4f8743f89f
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-15 16:25:19 +0200

    some correct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Profile from './components/profile/Profile';
 import user from './components/jsonfiles/user';
+
 import Statistics from './components/statistics/Statistics';
 import data from './components/jsonfiles/data';
+
 import FriendList from './components/friend/FriendList'
 import friends from './components/jsonfiles/friends';
+
 import TransactionHistory from './components/transition/TransactionHistory';
 import transactions from './components/jsonfiles/transactions';
 
