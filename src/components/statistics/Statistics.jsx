1c5bc622ca7f7d4f6f18c6fc75a163d87c175e60
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-16 00:15:24 +0200

    add some styles for statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,16 +1,17 @@
+import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
 export default function Statistics({ title, stats }) {
   return (
-    <section class="statistics">
-      <h2 class="title">{title}</h2>
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
 
-      <ul class="stat-list">
+      <ul className={css.stat}>
         {stats.map(stat => {
           return (
-            <li key={stat.id} class="item">
-              <span class="label">{stat.label}</span>
-              <span class="percentage">{stat.percentage}%</span>
+            <li key={stat.id} className={css.item}>
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}%</span>
             </li>
           );
         })}
