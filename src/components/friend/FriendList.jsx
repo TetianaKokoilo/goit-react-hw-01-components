3665e0cd3ff4cebb7fb936aaee032bcf8d8442ec
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-15 15:43:17 +0200

    add status isOnline for FriendList

--- src/components/friend/FriendList.jsx
+++ src/components/friend/FriendList.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
 export default function FriendList({ friends }) {
+
   return (
     <ul class="friend-list">
       {friends.map(friend => {
         return (
             <li key={friend.id} class="item">
-                <span class="status">{friend.isOnline}</span>
+                <span class="status"> {friend.isOnline === true ? 'online' : 'ofline'}</span>
+
             <img class="avatar" src={friend.avatar} alt={friend.name} width="48" />
                 <p class="name">{friend.name}</p>
           </li>
