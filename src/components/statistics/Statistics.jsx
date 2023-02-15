50f8622197cfa73874e64ffbcf248a7c8b92d384
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-16 00:54:51 +0200

    add styles for statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
-export default function Statistics({ title, stats }) {
+export default function Statistics({ title, stats, variant }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.stat}>
         {stats.map(stat => {
